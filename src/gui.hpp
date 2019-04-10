8d71b7209410c327db56c3fcaf76ec69b6fa231d
Volti-Z <45297199+Volti-Z@users.noreply.github.com>
2019-04-10 20:54:45 +0200

    Start to implement the Sprites from the SpriteSheet.

---
+++ src/gui.hpp
@@ -0,0 +1,19 @@
+#ifndef __UNIVERSAL_UPDATER__
+#define __UNIVERSAL_UPDATER__
+
+//Add the Include Paths.
+#include <citro3d.h>
+#include <citro2d.h>
+
+#define SCREEN_WIDTH  400
+#define SCREEN_HEIGHT 240
+
+typedef enum {
+	TOP_BACKGROUND,
+	DOT,
+	LOADING_BACKGROUND,
+	PAGE_BUTTON,
+	PAGE_BUTTON_SMALL,
+	TOP_BACKGROUND,
+} Gui_Sprites;
+
