11eeac426bd05e89500525d02585002875055630
SuperSaiyajinVoltZ <47382115+SuperSaiyajinVoltZ@users.noreply.github.com>
2019-04-11 23:20:24 +0200

    Add Missing Files.

---
+++ include/utils/common.h
@@ -0,0 +1,30 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+// Include Paths.
+#include <3ds.h>
+#include <algorithm>
+#include <codecvt>
+#include <fstream>
+#include <locale>
+#include <memory>
+#include <sstream>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <string>
+#include <sys/stat.h>
+#include <vector>
+#include "datetime.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "sha256.h"
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif
---
+++ include/utils/datetime.h
@@ -0,0 +1,10 @@
+#ifndef DATETIME_H
+#define DATETIME_H
+
+// Include Paths.
+#include "common.h"
+
+std::string getTime(void);
+std::string getPathDateTime(void);
+
+#endif
