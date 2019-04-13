47853558ef3a504c3377007fa9853dcee5f15f52
Volti-Z <45297199+Volti-Z@users.noreply.github.com>
2019-04-14 01:02:23 +0200

    Oops.. Forgot to delete the Custom Theme Path. (But it's planned though.)

--- source/main.cpp
+++ source/main.cpp
@@ -174,7 +174,6 @@ int main()
 	mkdir("sdmc:/3ds", 0777);	// For DSP dump
 	mkdir("sdmc:/Universal-Updater", 0777); //main Path.
 	mkdir("sdmc:/Universal-Updater/Music", 0777); //MP3 Path.
-	mkdir("sdmc:/Universal-Updater/customTheme", 0777); //Custom Theme Path!
 
 	pp2d_init();
 	
