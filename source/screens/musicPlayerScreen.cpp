7621a4f46d469d4ac30f7cb1a40d6e00daba01f5
SuperSaiyajinVoltZ <>
2019-05-19 17:45:03 +0200

    Allow, to play the Music, even if the 3DS is in `sleepMode`.
    So you can hear the Music even if you are on the Go with your HeadPhones! :D

--- source/screens/musicPlayerScreen.cpp
+++ source/screens/musicPlayerScreen.cpp
@@ -77,6 +77,7 @@ void drawMusicPlayerUI(void) {
 			playbackInfo_t playbackInfo;
 			changeFile(dirContents[selectedFile].name.c_str(), &playbackInfo);
 			screenMode = musicPlayScreen;
+			aptSetSleepAllowed(false);
 		}
 	} else if (hDown & KEY_B) {
 		chdir("..");
