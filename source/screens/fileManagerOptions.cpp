e2ffc26f7af3407c05689f845d64fa0aeb80359d
Epicpkmn11 <epicpkmn11@outlook.com>
2019-06-21 22:31:13 -0500

    Use a for loop for the file manager buttons

--- source/screens/fileManagerOptions.cpp
+++ source/screens/fileManagerOptions.cpp
@@ -27,6 +27,7 @@
 #include "screens/screenCommon.hpp"
 #include <algorithm>
 #include <fstream>
+#include <string>
 #include <unistd.h>
 #include <vector>
 #include "fileBrowse.h"
@@ -36,47 +37,39 @@ extern "C" {
 }
 
 struct ButtonPos {
-    int x;
-    int y;
-    int w;
-    int h;
-	int link;
+	int x;
+	int y;
+	int w;
+	int h;
+	std::string text;
 };
 
-ButtonPos FunctionPos[] = {
-    {59, 70, 93, 35, -1},
-    {165, 70, 93, 35, -1},
-    {59, 110, 93, 35, -1},
-	{165, 110, 93, 35, -1},
-	{59, 150, 93, 35, -1},
-	{165, 150, 93, 35, -1},
+ButtonPos functionPos[] = {
+	{59, 70, 93, 35, "Rename"},
+	{165, 70, 93, 35, "Delete"},
+	{59, 110, 93, 35, ""},
+	{165, 110, 93, 35, ""},
+	{59, 150, 93, 35, ""},
+	{165, 150, 93, 35, ""},
 };
 
 
 
 bool displayActionBox(void) {
-    C3D_FrameBegin(C3D_FRAME_SYNCDRAW);
-    set_screen(bottom);
+	C3D_FrameBegin(C3D_FRAME_SYNCDRAW);
+	set_screen(bottom);
 	//Gui::Draw_ImageBlend(sprites_actionBox_idx, 54, 30, settings.universal.bars);
 	C2D_DrawRectSolid(54, 30, 0.5f, 211, 180, settings.universal.bars);
 	Gui::sprite(sprites_actionBox_idx, 54, 30);
 
 	// Buttons.
-	Gui::sprite(sprites_FileManagerButton_idx, FunctionPos[0].x, FunctionPos[0].y);
-	draw_text(FunctionPos[0].x+12, FunctionPos[0].y+10, 0.6f, 0.6f, WHITE, "Rename");
-	Gui::sprite(sprites_FileManagerButton_idx, FunctionPos[1].x, FunctionPos[1].y);
-	draw_text(FunctionPos[1].x+12, FunctionPos[1].y+10, 0.6f, 0.6f, WHITE, "Delete");
-	Gui::sprite(sprites_FileManagerButton_idx, FunctionPos[2].x, FunctionPos[2].y);
-	//draw_text(FunctionPos[2].x+12, FunctionPos[2].y+10, 0.6f, 0.6f, WHITE, "");
-	Gui::sprite(sprites_FileManagerButton_idx, FunctionPos[3].x, FunctionPos[3].y);
-	//draw_text(FunctionPos[3].x+12, FunctionPos[3].y+10, 0.6f, 0.6f, WHITE, "");
-	Gui::sprite(sprites_FileManagerButton_idx, FunctionPos[4].x, FunctionPos[4].y);
-	//draw_text(FunctionPos[4].x+12, FunctionPos[4].y+10, 0.6f, 0.6f, WHITE, "");
-	Gui::sprite(sprites_FileManagerButton_idx, FunctionPos[5].x, FunctionPos[5].y);
-	//draw_text(FunctionPos[5].x+12, FunctionPos[5].y+10, 0.6f, 0.6f, WHITE, "");
+	for(uint i=0; i<(sizeof(functionPos)/sizeof(functionPos[0]));i++) {
+		Gui::sprite(sprites_FileManagerButton_idx, functionPos[i].x, functionPos[i].y);
+		draw_text(functionPos[i].x+12, functionPos[i].y+10, 0.6f, 0.6f, WHITE, functionPos[i].text.c_str());
+	}
 
-    Draw_EndFrame();
-    while(1) {
+	Draw_EndFrame();
+	while(1) {
 		gspWaitForVBlank();
 		hidScanInput();
 		if(keysDown() & KEY_A) {
@@ -84,5 +77,5 @@ bool displayActionBox(void) {
 		} else if(keysDown() & KEY_B) {
 			return false;
 		}
-    }
+	}
 }
