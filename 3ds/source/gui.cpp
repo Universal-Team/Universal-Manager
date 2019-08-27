d650e24af6137cc7668396478abf43fcbf43a264
SuperSaiyajinVoltZ <47382115+SuperSaiyajinVoltZ@users.noreply.github.com>
2019-08-27 23:43:01 +0200

    Add latest Text changes from LeafEdit here as well.

--- 3ds/source/gui.cpp
+++ 3ds/source/gui.cpp
@@ -340,6 +340,7 @@ void Gui::DrawString(float x, float y, float size, u32 color, std::string Text)
 
 // Get String or Text Width.
 float Gui::GetStringWidth(float size, std::string Text) {
+    findAndReplaceAll(Text, "\\n", "\n");
 	float width = 0;
 	GetStringSize(size, &width, NULL, Text);
 	return width;
@@ -347,6 +348,7 @@ float Gui::GetStringWidth(float size, std::string Text) {
 
 // Get String or Text Size.
 void Gui::GetStringSize(float size, float *width, float *height, std::string Text) {
+    findAndReplaceAll(Text, "\\n", "\n");
 	C2D_Text c2d_text;
     C2D_TextFontParse(&c2d_text, systemFont, sizeBuf, Text.c_str());
 	C2D_TextGetDimensions(&c2d_text, size, size, width, height);
@@ -355,6 +357,7 @@ void Gui::GetStringSize(float size, float *width, float *height, std::string Tex
 
 // Get String or Text Height.
 float Gui::GetStringHeight(float size, std::string Text) {
+    findAndReplaceAll(Text, "\\n", "\n");
 	float height = 0;
 	GetStringSize(size, NULL, &height, Text.c_str());
 	return height;
