de285beff6c8fa81f2db0c926ddc20c9e0f84b74
SuperSaiyajinVoltZ <47382115+SuperSaiyajinVoltZ@users.noreply.github.com>
2019-09-20 21:57:50 +0200

    The Text was too long. Shorten it!

--- 3ds/source/screens/creditsScreen.cpp
+++ 3ds/source/screens/creditsScreen.cpp
@@ -240,7 +240,7 @@ void Credits::Draw(void) const
 	Gui::DrawString(50, 0, 0.72f, WHITE, "Welcome to Universal-Manager!");
 	drawCreditsDialogs();
 	drawButtons();
-	Gui::DrawString(0, 218, 0.55, WHITE, "Join our Discord here: https://discord.gg/KDJCfGF", 320);
+	Gui::DrawString(5, 220, 0.55, WHITE, "Join our Discord: https://discord.gg/KDJCfGF", 320);
 }
 
 void Credits::Logic(u32 hDown, u32 hHeld, touchPosition touch) {
